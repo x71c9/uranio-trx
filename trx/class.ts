a368294fc623ede03d90b556d45406d78d1ea9b6
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-08-16 14:23:20 +0200

    developing trx.

--- trx/class.ts
+++ trx/class.ts
@@ -4,21 +4,22 @@
  * @packageDocumentation
  */
 
-import {urn_util, urn_log, urn_return, urn_response, urn_exception} from 'urn-lib';
+// import {urn_util, urn_log, urn_return, urn_response, urn_exception} from 'urn-lib';
+import {urn_util, urn_log, urn_response, urn_exception} from 'urn-lib';
 
-const urn_ret = urn_return.create(urn_log.util.return_injector);
+// const urn_ret = urn_return.create(urn_log.util.return_injector);
 
 const urn_exc = urn_exception.init(`TRX`, `TRX module.`);
 
-import * as client_types from '../cln/types';
-
 import {atom_book} from 'uranio-books-client/atom';
 
-import {api_book} from 'uranio-books-client/api';
+// import {api_book} from 'uranio-books-client/api';
+
+import urn_api_client from 'uranio-api/client';
+
+import * as client_types from '../cln/types';
 
 import {create as create_raw} from '../raw/';
-
-import {default_routes} from '../api/routes/public';
 
 @urn_log.util.decorators.debug_constructor
 @urn_log.util.decorators.debug_methods
@@ -30,13 +31,45 @@ class TRX {
 		this.raw = create_raw();
 	}
 	
-	public async call_route(route_name:string)
-			:Promise<urn_response.General<any, any>>{
+	public route(route_name:string)
+			:(...args:any) => Promise<urn_response.General<any, any>>{
 		_check_atom(this.atom_name);
-		_check_route(this.atom_name, route_name);
-		// ----
-		// TODO implement api default route and call it
-		// ----
+		const route = _get_route(this.atom_name, route_name);
+		const splitted_url = route.url.split('/');
+		const params:string[] = [];
+		for(const split of splitted_url){
+			if(split.includes(':')){
+				const splitted_split = split.split(':');
+				if(splitted_split.length !== 2){
+					throw urn_exc.create(
+						`INVALID_ROUTE_URL`,
+						`Invalid Route URL format [${route.url}]`
+					);
+				}
+				const param_name = splitted_split[1];
+				params.push(param_name);
+			}
+		}
+		switch(params.length){
+			case 0:{
+				return async () => {
+					return await this.raw.get('/');
+				};
+			}
+			case 1:{
+				return async (p:string) => {
+					const url = route.url.replace(`/:${params[0]}`, p);
+					return await this.raw.get(url);
+				};
+			}
+			default:
+			case 2:{
+				return async (p:string, q:string) => {
+					const url = route.url.replace(`/:${params[0]}`, p).replace(`/:${params[1]}`, q);
+					return await this.raw.get(url);
+				};
+			}
+		}
 	}
 	
 }
@@ -52,18 +85,19 @@ function _check_atom(atom_name:client_types.AtomName)
 	);
 }
 
-function _check_route(atom_name:client_types.AtomName, route_name:string)
-		:true{
-	if(urn_util.object.has_key(default_routes, route_name)){
-		return true;
-	}
-	if(urn_util.object.has_key(api_book[atom_name], route_name)){
-		return true;
-	}
-	throw urn_exc.create_not_found(
-		`TRXROUTE_UNDEFINED`,
-		`TRX Route not found for atom [${atom_name}] route [${route_name}].`
-	);
+function _get_route(atom_name:client_types.AtomName, route_name:string)
+		:client_types.Book.Definition.Api.Routes.Route{
+	return urn_api_client.routes.route_def(atom_name, route_name);
+	// if(urn_util.object.has_key(urn_api_client.routes.default_routes, route_name)){
+	//   return urn_api_client.routes.default_routes[route_name];
+	// }
+	// if(urn_util.object.has_key(api_book[atom_name], route_name)){
+	//   return true;
+	// }
+	// throw urn_exc.create_not_found(
+	//   `TRXROUTE_UNDEFINED`,
+	//   `TRX Route not found for atom [${atom_name}] route [${route_name}].`
+	// );
 }
 
 export type TRXInstance = InstanceType<typeof TRX>;
