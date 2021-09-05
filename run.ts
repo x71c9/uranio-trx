10509ff02db043bf4948f4620e5e470f339aa9e9
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-09-05 19:39:11 +0200

    updated api and run.

--- run.ts
+++ run.ts
@@ -18,26 +18,29 @@ console.log(uranio.types.AuthAction);
 const service = uranio.api.service.create();
 service.listen(8888, () => {
 	
-	setTimeout(async () => {
-		// const karts = uranio.base.create('kart');
-		// const mycustom = karts.hook('mycustom');
-		// const args:uranio.types.HookArguments<'kart', 'mycustom'> = {
-		//   params: {
-		//     id: '611bedc525e7b32c3f6415d8',
-		//     date: 'kajflkdsa'
-		//   },
-		//   query: {
-		//     sick: {
-		//       email: '',
-		//       email2: 'SS'
-		//     }
-		//   }
-		// };
-		// mycustom(args).then(r => console.log(r)).catch(e => console.error(e));
-		// const a = await uranio.hooks.karts.mycustom('611bedc525e7b32c3f6415d8', 'djkhd');
-		// console.log(a);
-		uranio.hooks.products.delete('6128faa891988267692430a7').then(e => console.log(e)).catch(e => console.error(e));
-	}, 3000);
+	// const pro_bll = uranio.core.bll.basic.create('product');
+	// pro_bll.count({}).then(c => console.log(c));
+
+	// setTimeout(async () => {
+	//   // const karts = uranio.base.create('kart');
+	//   // const mycustom = karts.hook('mycustom');
+	//   // const args:uranio.types.HookArguments<'kart', 'mycustom'> = {
+	//   //   params: {
+	//   //     id: '611bedc525e7b32c3f6415d8',
+	//   //     date: 'kajflkdsa'
+	//   //   },
+	//   //   query: {
+	//   //     sick: {
+	//   //       email: '',
+	//   //       email2: 'SS'
+	//   //     }
+	//   //   }
+	//   // };
+	//   // mycustom(args).then(r => console.log(r)).catch(e => console.error(e));
+	//   // const a = await uranio.hooks.karts.mycustom('611bedc525e7b32c3f6415d8', 'djkhd');
+	//   // console.log(a);
+	//   uranio.hooks.products.delete('6128faa891988267692430a7').then(e => console.log(e)).catch(e => console.error(e));
+	// }, 3000);
 	
 });
 
