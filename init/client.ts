c8ac4eaa1551db40c2c45362750df1e91a1c4a27
Andrea Reni <mail@nbl7.net>
2021-12-20 18:43:52 +0100

    added init client.

---
+++ init/client.ts
@@ -0,0 +1,19 @@
+/**
+ * Init module
+ *
+ * @packageDocumentation
+ */
+
+import {trx_client_config} from '../cln/defaults';
+
+import * as types from '../cln/types';
+
+import * as conf from '../conf/client';
+
+export function init(config:types.ClientConfiguration)
+		:void{
+	
+	conf.set(trx_client_config, config);
+	
+	conf.set_initialize(true);
+}
