c40fb0d6145ced1a44b9e69ce6a77d29167c0d55
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-01 13:39:24 +0100

    preparing repo for atoms client server.

---
+++ src/client/register.ts
@@ -0,0 +1,9 @@
+/**
+ * API register required atoms module
+ *
+ * @packageDocumentation
+ */
+
+// export * from '../atoms/client/[atom_name]/index.ts';
+// export * from '../atoms/client/[atom_name]/routes/[route_name].ts';
+export {};
