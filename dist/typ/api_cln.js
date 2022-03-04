778378888bebba1864d21c3857c97c6bf5198094
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-04 19:03:15 +0100

    some compilation changed api types.

--- dist/typ/api_cln.js
+++ dist/typ/api_cln.js
@@ -5,14 +5,11 @@
  * @packageDocumentation
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Passport = exports.AuthAction = exports.PermissionType = exports.SecurityType = exports.PropertyType = exports.RouteMethod = exports.RouteParam = exports.Api = void 0;
+exports.AuthAction = exports.PermissionType = exports.SecurityType = exports.PropertyType = exports.RouteMethod = void 0;
 var types_1 = require("uranio-api/client/types");
-Object.defineProperty(exports, "Api", { enumerable: true, get: function () { return types_1.Api; } });
-Object.defineProperty(exports, "RouteParam", { enumerable: true, get: function () { return types_1.RouteParam; } });
 Object.defineProperty(exports, "RouteMethod", { enumerable: true, get: function () { return types_1.RouteMethod; } });
 Object.defineProperty(exports, "PropertyType", { enumerable: true, get: function () { return types_1.PropertyType; } });
 Object.defineProperty(exports, "SecurityType", { enumerable: true, get: function () { return types_1.SecurityType; } });
 Object.defineProperty(exports, "PermissionType", { enumerable: true, get: function () { return types_1.PermissionType; } });
 Object.defineProperty(exports, "AuthAction", { enumerable: true, get: function () { return types_1.AuthAction; } });
-Object.defineProperty(exports, "Passport", { enumerable: true, get: function () { return types_1.Passport; } });
 //# sourceMappingURL=api_cln.js.map
