2ebddd5bd19191c422e0abc79c8a20582bc4ba90
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-04-15 12:46:16 +0200

    compiled hooks from admin.

--- dist/dev.js
+++ dist/dev.js
@@ -14,7 +14,9 @@ console.log(server_1.default.env.get_all());
 console.log(server_1.default.conf.get_all());
 const service = server_1.default.api.service.create();
 service.listen(async () => {
-    const res = await server_1.default.hooks.media.count();
+    // const res = await uranio.hooks.media.count();
+    // console.log(res);
+    const res = await server_1.default.hooks.users.find_id('61e9980a5bea5d4f9044f19b', { query: { options: { depth: 1 } } });
     console.log(res);
 });
 //# sourceMappingURL=dev.js.map
--- src/dev.ts
+++ src/dev.ts
@@ -12,6 +12,8 @@ console.log(uranio.conf.get_all());
 
 const service = uranio.api.service.create();
 service.listen(async () => {
-	const res = await uranio.hooks.media.count();
+	// const res = await uranio.hooks.media.count();
+	// console.log(res);
+	const res = await uranio.hooks.users.find_id('61e9980a5bea5d4f9044f19b', {query: {options: {depth: 1}}});
 	console.log(res);
 });
